b7ff189094577f3b7ce96775fde668b58876d7cc
Icon <you@example.com>
2016-08-21 20:06:35 +0800

    alternate players

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('Hello frontend');
+//
+// set grid to listen to click event, based on turn append innerHTML X or O into the grid.
+
+// var playerOne = 'X'
+// var playerTwo = 'O'
+var currentPlayer = 'X'
+
+function play(id) {
+  return function() {
+    if (currentPlayer === 'X') {
+      document.getElementById(id).innerHTML = currentPlayer;
+      currentPlayer = 'O'
+    }
+    else {
+      document.getElementById(id).innerHTML = currentPlayer;
+      currentPlayer = 'X'
+    }
+  }
+}
+
+document.getElementById("one").addEventListener("click", play('one'))
+document.getElementById("two").addEventListener("click", play('two'))
+document.getElementById("three").addEventListener("click", play('three'))
+document.getElementById("four").addEventListener("click", play('four'))
+document.getElementById("five").addEventListener("click", play('five'))
+document.getElementById("six").addEventListener("click", play('six'))
+document.getElementById("seven").addEventListener("click", play('seven'))
+document.getElementById("eight").addEventListener("click", play('eight'))
+document.getElementById("nine").addEventListener("click", play('nine'))
