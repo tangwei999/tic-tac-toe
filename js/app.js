61033bd1fcf0eb5f0890448ee4c3e3f9ae9ac3fa
Icon <you@example.com>
2016-08-22 10:14:00 +0800

    first commit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
 //
 // set grid to listen to click event, based on turn append innerHTML X or O into the grid.
 
-// var playerOne = 'X'
-// var playerTwo = 'O'
+
 var currentPlayer = 'X'
+playingArray = [null, null, null, null, null, null, null, null, null]
+
+// run a function that converts id to index of array
+
+// Constantly check if the grids have a winning combination before going back to alternate the player.
+//
+// Comparing the string inside the grids.
+
+function checkWinner() {
+  if (playingArray[0] === playingArray[1] && playingArray[0] === playingArray[2]) {
+      document.getElementsByTagName("h2")[0].innerHTML = "You Won!"
+    }
+}
+
 
 function play(id) {
   return function() {
     if (currentPlayer === 'X') {
       document.getElementById(id).innerHTML = currentPlayer;
-      currentPlayer = 'O'
+      currentPlayer = 'O';
+      // replace element in playingArray
+      checkWinner()
     }
     else {
       document.getElementById(id).innerHTML = currentPlayer;
-      currentPlayer = 'X'
+      currentPlayer = 'X';
+      checkWinner()
     }
   }
 }
 
-document.getElementById("one").addEventListener("click", play('one'))
-document.getElementById("two").addEventListener("click", play('two'))
-document.getElementById("three").addEventListener("click", play('three'))
-document.getElementById("four").addEventListener("click", play('four'))
-document.getElementById("five").addEventListener("click", play('five'))
-document.getElementById("six").addEventListener("click", play('six'))
-document.getElementById("seven").addEventListener("click", play('seven'))
-document.getElementById("eight").addEventListener("click", play('eight'))
-document.getElementById("nine").addEventListener("click", play('nine'))
+document.getElementById("g0").addEventListener("click", play('g0'))
+document.getElementById("g1").addEventListener("click", play('g1'))
+document.getElementById("g2").addEventListener("click", play('g2'))
+document.getElementById("g3").addEventListener("click", play('g3'))
+document.getElementById("g4").addEventListener("click", play('g4'))
+document.getElementById("g5").addEventListener("click", play('g5'))
+document.getElementById("g6").addEventListener("click", play('g6'))
+document.getElementById("g7").addEventListener("click", play('g7'))
+document.getElementById("g8").addEventListener("click", play('g8'))
